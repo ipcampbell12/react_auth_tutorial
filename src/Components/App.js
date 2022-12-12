1633ed8ebea43d92553ca9d9700acdbf1236e5e3
ipcampbell12 <ipcampbell12@gmail.com>
2022-12-12 11:18:16 -0800

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,32 @@
+import Signup from "./Signup";
+import { Container } from 'react-bootstrap'
+import { AuthProvider } from "../Context/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+
+
+function App() {
+  return (
+
+
+    <Container className="d-flex align-items-center justify-conten-center " style={{ minHeight: "100vh" }}>
+
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route exact path="/" element={<Dashboard />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+
+      </div>
+    </Container>
+
+  );
+}
+
+export default App;
